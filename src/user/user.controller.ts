94e20fd5b6150d2682eddfc6509bc17956459bde
m.shymechko <shymechko.m.a@gmail.com>
2025-01-17 20:09:22 +0200

    me endpoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -16,6 +16,27 @@ import { User } from 'src/schemas/user.schema';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  // @Get('me')
+  // @UseGuards(AuthGuard('AzureAD')) // Використання стратегії 'AzureAD'
+  // async getCurrentUser(@Req() req: Request): Promise<User> {
+  //     // payload отримується через стратегію
+  //     const userPayload = req.user;
+
+  //     if (!userPayload) {
+  //         throw new HttpException('User not authenticated', HttpStatus.UNAUTHORIZED);
+  //     }
+
+  //     // Отримання userId із payload (можливо, це буде `sub` або інший унікальний ідентифікатор)
+  //     const userId = userPayload.sub;
+
+  //     const user = await this.userService.getUserById(userId);
+  //     if (!user) {
+  //         throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+  //     }
+
+  //     return user;
+  // }
+
   @Get()
   getAll(): Promise<User[]> {
     return this.userService.getAll();
