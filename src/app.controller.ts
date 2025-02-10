6e1b89c3ad29b34023aa1e7051ae097aa98773e2
m.shymechko <shymechko.m.a@gmail.com>
2025-02-10 12:38:04 +0200

    test 1

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Query,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiQuery, ApiResponse } from '@nestjs/swagger';
+import { ApiOperation } from '@nestjs/swagger';
+import axios from 'axios';
 
 @Controller()
 export class AppController {
@@ -8,5 +17,31 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('fetch-html')
+  @ApiOperation({ summary: 'Fetch HTML from a given URL' })
+  @ApiQuery({
+    name: 'url',
+    required: true,
+    description: 'The URL to fetch HTML from',
+  })
+  @ApiResponse({ status: 200, description: 'Returns HTML content' })
+  @ApiResponse({ status: 400, description: 'URL is required' })
+  @ApiResponse({ status: 500, description: 'Failed to fetch HTML' })
+  async fetchHtml(@Query('url') url: string): Promise<string> {
+    if (!url) {
+      throw new HttpException('URL is required', HttpStatus.BAD_REQUEST);
+    }
+
+    try {
+      const response = await axios.get(url);
+      return response.data; // Повертаємо HTML-код
+    } catch {
+      throw new HttpException(
+        'Failed to fetch HTML',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
